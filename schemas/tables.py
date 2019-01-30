00bfdbfd743c9713a789c4a1b1708fa4bdf62e77
Cristiano Singulani <crisingulani@gmail.com>
2019-01-30 14:48:02 -0200

    Added productLog node in the process endpoint. Added DACHS url node in the product endpoint. Added comments endpoint.

--- schemas/tables.py
+++ schemas/tables.py
@@ -4,23 +4,33 @@ from graphene_sqlalchemy import SQLAlchemyObjectType
 from models import (
     Tables as TablesModel
 )
+import os
+
+DACHS_URL = os.getenv('DACHS_URL')
 
 
 class TablesAttribute():
-    user_id = Int()
-    user_name = String()
-    email_address = String()
-    display_name = String()
-    created = DateTime()
-    affiliation = String()
-    status = String()
-    url_photo = String()
-    about = String()
+    table_id = Int()
+    schema_name = String()
+    table_name = String()
+    dachs_url = String()
 
 
-class Tables(SQLAlchemyObjectType):
+class Tables(SQLAlchemyObjectType, TablesAttribute):
     """Tables node"""
 
     class Meta:
         model = TablesModel
         interfaces = (relay.Node,)
+
+
+    def resolve_dachs_url(self, info):
+        """ Mount link to DACHS service """
+
+        return os.path.join(
+            DACHS_URL,
+            "%(schema)s/%(table)s/q/form" % {
+                "schema": self.schema_name,
+                "table": self.table_name
+            }
+        )
